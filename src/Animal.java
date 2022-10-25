337012d872f130a218b5dd9daa06e0180d4b74e6
Stuart <stuartmcmillan100@hotmail.com>
2022-10-25 14:20:29 +0100

    Updated Animal base class.

--- src/Animal.java
+++ src/Animal.java
@@ -1,2 +1,18 @@
 public class Animal {
+
+    private String name;
+    private float height, length, width;
+    private float weight;
+
+
+    public Animal() {
+
+    }
+
+    public Animal(String name, float height, float length, float width) {
+        this.name = name;
+        this.height = height;
+        this.length = length;
+        this.width = width;
+    }
 }
